7dd69de3370a1377f6a5957ab7744bbfbbac9a1d
warllencs <warllencs@gmail.com>
2016-03-18 18:30:29 -0300

    service

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,5 +1,41 @@
 angular.module('aplicacao',['ngRoute','naif.base64'])
+.service( 'serverREST', function($http){
+	return {
+		"token": JSON.stringify({"token":"b73ba53c34ca234"}),
+		"enviar":function(rota){
+			return {
+			    method: 'POST',
+			    headers: {'Content-Type': undefined },
+			    url : rota,
+			    data : null
+			};
+		},
+		"listaHttp": function(rota){	
+		 	return $http(this.enviar(rota)).then(function successCallback(response) {	
+		 		return response.data;
+		    }, function errorCallback(response) {
+		    	return response;	
+		  	});
+ 		},
+ 		"salvaHttp": function(rota,obj){	
+ 			var req = this.enviar(rota);
+	 		req.data = JSON.stringify(obj);
+		 	$http(req).then(function successCallback(response) {
+		 		console.log(response);
+		    }, function errorCallback(response) {
+		    	console.log(response);	
+		  	});
+ 		},
+ 		"apagaHttp":function(rota,idApagar){
+		 	$http(this.enviar(rota+idApagar)).then(function successCallback(response) {
+		 		console.log(response);
+		    }, function errorCallback(response) {
+		    	console.log(response);	
+		  	});
+ 		}
 
+	}
+})
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when('/home', {
@@ -21,62 +57,21 @@ angular.module('aplicacao',['ngRoute','naif.base64'])
             redirectTo: '/home'
         });
 }])
-.controller('produtos',function($scope,$http){
+.controller('produtos',function($scope,serverREST){
 
 	$scope.reset = {"listar":false,"editar":false,"inserir":false};
 	$scope.estado = angular.copy($scope.reset);
 	$scope.urlPadrao = "https://phprest-nellrcs.c9users.io/app_loja/";
 
-
 	//OFF
 	$scope.produtos = [];
 
-	/* serviço */
-	
-	var req = {
-	    method: 'POST',
-	    headers: {'Content-Type': undefined }
-	};
 
- 	$scope.listaHttp = function() 
- 	{	
- 		req.url = $scope.urlPadrao+"produto/";
- 		req.data = JSON.stringify({"token":"b73ba53c34ca234"});
-
-	 	$http(req).then(function successCallback(response) {
-	 		$scope.produtos = response.data;
-	    }, function errorCallback(response) {
-	    	console.log(response);	
-	  	});
- 	}
-
- 	$scope.salvaHttp = function(obj) 
- 	{
- 		req.url = $scope.urlPadrao+"/produto/manter";
- 		req.data = JSON.stringify(obj);
-	 	$http(req).then(function successCallback(response) {
-	 		//$scope.produtos = response.data;
-	 		console.log(response);
-	 		$scope.listaHttp();
-	    }, function errorCallback(response) {
-	    	console.log(response);	
-	  	});
- 	}
-
- 	$scope.apagaHttp = function(idApagar) 
- 	{
- 		req.url = $scope.urlPadrao+"/produto/apagar/"+idApagar;
-	 	$http(req).then(function successCallback(response) {
-	 		//$scope.produtos = response.data;
-	 		$scope.listaHttp();
-	    }, function errorCallback(response) {
-	    	console.log(response);	
-	  	});
- 	}
-
-	/*  - - - - - - */
 	$scope.inicial = function(){
-		$scope.listaHttp();	
+		serverREST.listaHttp($scope.urlPadrao+'produto/')
+		 .then(function successCallback(data){
+		  		$scope.produtos = data;
+		 });
 		$scope.estado = angular.copy($scope.reset);
 		$scope.estado.listar = true;		
 	}
@@ -94,40 +89,43 @@ angular.module('aplicacao',['ngRoute','naif.base64'])
 	}
 
 	$scope.salvar = function(obj){
-
 		$scope.estado = angular.copy($scope.reset);
 		$scope.temp = angular.copy(obj);
-		//$scope.temp.foto_produto = $scope.temp.foto_produto.base64;
-		//$scope.produtos.push($scope.temp);
-		$scope.salvaHttp($scope.temp);
+		serverREST.salvaHttp($scope.urlPadrao+'produto/manter',$scope.temp);
+		$scope.inicial();
 		$scope.produto = null;
-		$scope.estado.listar = true;
 	}
 
 	$scope.apagar = function(obj){
-		console.log(obj);
-		$scope.apagaHttp(obj.ID)
-				
+		serverREST.apagaHttp($scope.urlPadrao+'produto/apagar/',obj.ID);
+		$scope.inicial();	
 	}
 
 })
-.controller('usuarios',function($scope){
+.controller('usuarios',function($scope,serverREST){
 
 	$scope.reset = {"listar":false,"editar":false,"inserir":false};
 	$scope.estado = angular.copy($scope.reset);
+	$scope.urlPadrao = "https://phprest-nellrcs.c9users.io/app_loja/";
+
 
 	//OFF
 	$scope.usuarios = [];
 
+
 	$scope.inicial = function(){
+		serverREST.listaHttp($scope.urlPadrao+'cliente/')
+		 .then(function successCallback(data){
+		  		$scope.usuarios = data;
+		 });
 		$scope.estado = angular.copy($scope.reset);
 		$scope.estado.listar = true;		
 	}
 	
 	$scope.inserir = function(){
-		$scope.usuario = null;
 		$scope.estado = angular.copy($scope.reset);
 		$scope.estado.inserir = true;
+		$scope.usuario = null;
 	}
 
 	$scope.editar = function(obj){
@@ -135,19 +133,18 @@ angular.module('aplicacao',['ngRoute','naif.base64'])
 		$scope.estado.editar = true;
 		$scope.usuario = angular.copy(obj);
 	}
-	
+
 	$scope.salvar = function(obj){
 		$scope.estado = angular.copy($scope.reset);
 		$scope.temp = angular.copy(obj);
-		
-		$scope.usuarios.push($scope.temp);
+		serverREST.salvaHttp($scope.urlPadrao+'cliente/manter',$scope.temp);
+		$scope.inicial();
 		$scope.usuario = null;
-		$scope.estado.listar = true;
-		console.log($scope.usuarios);
 	}
 
 	$scope.apagar = function(obj){
-		console.log(obj);
+		serverREST.apagaHttp($scope.urlPadrao+'cliente/apagar/',obj.ID);
+		$scope.inicial();	
 	}
 
 })
@@ -162,5 +159,4 @@ angular.module('aplicacao',['ngRoute','naif.base64'])
 		"emailCliente":"cliente@compra.com",
 		"dataCliente":"22/05/2016"
 	})
-
 })
